9580c0df3f5adf66f8b6b19e93c15441a85ba422
Daedra <foglzerika@gmail.com>
2020-06-01 10:33:07 -0500

    feat | setting up GraphQL

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const graphlHTTP = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql' , graphlHTTP({
+
+}));
 const port = `${process.env.PORT}`
 
 app.listen(port , () =>{
