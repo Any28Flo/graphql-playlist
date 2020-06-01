c65d2d459fa81a025459cd8ca9c9fe32492b4a87
Daedra <foglzerika@gmail.com>
2020-06-01 07:54:32 -0500

    feat | Creating gitignore file and creating server configuration

---
+++ server/app.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = `${process.env.PORT}`
+
+app.listen(port , () =>{
+    console.log(`Server running on port ${port}`)
+})
