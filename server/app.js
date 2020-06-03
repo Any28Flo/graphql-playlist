b257fdf52b504951b199e5fc821cfea28f02994b
Daedra <foglzerika@gmail.com>
2020-06-03 07:54:55 -0500

    feat | Create db conection

--- server/app.js
+++ server/app.js
@@ -2,10 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const graphlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const port = `${process.env.PORT}`
 
 const app = express();
+
+mongoose.connect(`${process.env.DB_CONECTION}` ,  {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once('open' , () =>{
+    console.log("Conection to the database sucess");
+})
 
 app.use('/graphql' , graphlHTTP({
     schema,
