5bc9cfb291aeaff7ffa9c8fefb5c0807bf874b81
Daedra <foglzerika@gmail.com>
2020-06-04 05:15:27 -0500

    ref | Refactoring mutation function to save books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -20,11 +20,11 @@ const {
 //Create dummy data
 /*let books = [
     { name : 'Name of the Wind' , genre : 'Fantasy' , id : '1', authorId : '1'},
-    { name : 'The Final Empire' , genre : 'Fantasy' , id : '2', authorId: '2'},
-    { name : 'The Long Earth' , genre : 'Sci-Fi' , id : '3' , authorId: '3'},
-    { name : 'The Hero of Ages' , genre : ' Fantasy' , id : '4' , authorId: '2'},
-    { name : 'The Colour of Magic' , genre : 'Fantasy' , id: '5' , authorId: '3'},
-    { name : 'The Light Fantastic' , genre : 'Fantasy' , id :'6' , authorId: '3'}
+    { name : '' , genre : 'Fantasy' , id : '2', authorId: '2'},
+    { name : '' , genre : '' , id : '3' , authorId: '3'},
+    { name : '' , genre : ' ' , id : '4' , authorId: '2'},
+    { name : '' , genre : 'Fantasy' , id: '5' , authorId: '3'},
+    { name : '' , genre : 'Fantasy' , id :'6' , authorId: '3'}
 
 ];
 
