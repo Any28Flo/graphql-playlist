1f6b78f11a17175a77038375ecdf784411f29181
Daedra <foglzerika@gmail.com>
2020-06-04 19:27:29 -0500

    feat | Creating BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import BookList from './components/BookList'
 function App() {
   return (
     <div className="main">
         <h2>Bienvenido a mi app libreria </h2>
+        <BookList/>
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () =>{
+    return(
+        <div className="bookList">
+            <h1>BookList</h1>
+            <ul>
+                <li>Book_1</li>
+            </ul>
+        </div>
+    )
+}
+export  default BookList;
