047a8721d147a923a972d41b56aab449b7b50bda
Daedra <foglzerika@gmail.com>
2020-06-04 19:42:14 -0500

    feat | Apollo client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
 import BookList from './components/BookList'
-function App() {
-  return (
-    <div className="main">
-        <h2>Bienvenido a mi app libreria </h2>
-        <BookList/>
-    </div>
-  );
-}
+
+//apollo client setup
+const client = new ApolloClient({
+    uri: process.env.URL
+});
 
 export default App;
+
+//Components
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <div className="main">
+                <h2>Bienvenido a mi app libreria </h2>
+                <BookList/>
+            </div>
+        </ApolloProvider>
+
+);
+}
